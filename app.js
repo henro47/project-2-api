434b68ea756e345a3e4a10f18b911d5c3f34a01b
Henro Potgieter <henro47@gmail.com>
2020-10-06 18:42:42 +0200

    added CORS errors

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','*');
+
+    if(req.method === 'OPTIONS')
+    {
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
 app.use('/helloworld',helloWorldRoutes);
 
 app.use((req, res, next) => {
