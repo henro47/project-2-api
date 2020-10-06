d97804513078b2c2c292bfcaeae536d8c80e4c62
Henro Potgieter <henro47@gmail.com>
2020-10-06 18:23:17 +0200

    added morgan and error handling

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const helloWorldRoutes = require('./api/routes/helloworld');
 
+app.use(morgan('dev'));
+
 app.use('/helloworld',helloWorldRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found!');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500) ;
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
