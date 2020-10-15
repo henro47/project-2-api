45101385c5ac3f37c9b1f97be43189a4d97782da
Henro Potgieter <henro47@gmail.com>
2020-10-15 14:58:06 +0200

    Update userSchema.js

--- api/models/userSchema.js
+++ api/models/userSchema.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema =  mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    idNum: {type: String, required: true},
-    fName: {type: String, required: true},
-    lName: {type: String, required: true},
-    email: {type: String, required: true}
+    idNum: String,//{type: String, required: true},
+    fName: String,//{type: String, required: true},
+    lName: String,//{type: String, required: true},
+    email: String//{type: String, required: true}
 });
 
 module.exports = mongoose.model('User',userSchema);
