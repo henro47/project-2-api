74db2ba44ad7bf3eb9a1356d96335209b6f13871
Henro Potgieter <henro47@gmail.com>
2020-10-20 11:41:33 +0200

    added email validation to schema

--- api/models/userSchema.js
+++ api/models/userSchema.js
@@ -8,7 +8,11 @@ const userSchema =  mongoose.Schema({
     lName: {type: String, default: 'empty'},
     contact: {type: String, default: 'empty'},
     national: {type: String, default: 'empty'},
-    email: {type: String, required: true},
+    email: 
+        {type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
     password : {type: String, required: true}    
 });
 
--- api/routes/user.js
+++ api/routes/user.js
@@ -166,37 +166,47 @@ router.delete('/:userId', (req, res, next) =>{
 });
 
 router.post('/signup',(req, res, next) =>{
-    bycrypt.hash(req.body.password, 10, (err, hash) => {
-        if(err)
+    User.find({email: req.body.email}).exec()
+    .then(user => {
+        if(user.length > 0)
         {
-            return res.status(500).json({
-                error: err
+            return res.status(422).json({
+                message: 'User already exists in database'
             });
         }
-        else 
+        else
         {
-            const user = new User({
-                _id : new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-            .then(result => {
-                console.log(result);
-                res.status(201).json({
-                    message: 'user created'
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    error: err
-                });
+            bycrypt.hash(req.body.password, 10, (err, hash) => {
+                if(err)
+                {
+                    return res.status(500).json({
+                        error: err
+                    });
+                }
+                else 
+                {
+                    const user = new User({
+                        _id : new mongoose.Types.ObjectId(),
+                        email: req.body.email,
+                        password: hash
+                    });
+                    user.save()
+                    .then(result => {
+                        console.log(result);
+                        res.status(201).json({
+                            message: 'user created'
+                        });
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({
+                            error: err
+                        });
+                    });
+                }
             });
         }
-    });
-
-   
+    }) 
 });
 
 module.exports = router;
